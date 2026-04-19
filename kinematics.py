45aaef242125840fb08b63e5171224377e818dc1
Bartosz Węgiel <bawtek88@gmail.com>
2026-04-19 19:07:25 +0200

    Update kinematics.py

--- kinematics.py
+++ kinematics.py
@@ -8,24 +8,24 @@ def calculate_current_velocity(V0, a, t) -> pint.Quantity:
 
     Parameters
     ----------
-    V0 : float or pint.Quantity  
-        Initial velocity of an object (velocity at time `t=0`), by default in meters per second (m/s)  
-    a : float or pint.Quantity  
+    V0 : float or pint.Quantity    
+        Initial velocity of an object (velocity at time `t=0`), by default in meters per second (m/s).
+    a : float or pint.Quantity    
         Acceleration of an object, by default in meters per second squared (m/s^2).  
-    t : float or pint.Quantity  
+    t : float or pint.Quantity    
         Time at which we want to calculate the velocity, nonnegative, by default in seconds (s).  
 
     Returns
     -------
-    pint.Quantity  
+    pint.Quantity    
         Velocity at time `t`, in meters per second (m/s).
 
     Raises
     ------
-    ValueError  
+    ValueError    
         If time `t` is negative.  
-    pint.errors.DimensionalityError  
-        If input has incompatible units
+    pint.errors.DimensionalityError    
+        If input has incompatible units.  
     """
         
     if not isinstance(V0, _si.Quantity):
