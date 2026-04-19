594c546b7fdd541513b8a70d56ebf76c11f57f6a
Bartosz Węgiel <bawtek88@gmail.com>
2026-04-19 18:38:39 +0200

    Update kinematics.py

--- kinematics.py
+++ kinematics.py
@@ -1,44 +1,44 @@
 import pint
 
-si = pint.UnitRegistry()
+_si = pint.UnitRegistry()
 
 def calculate_current_velocity(V0, a, t) -> pint.Quantity:
+    """Calculate the velocity of an object at time `t`.  
+    Formula: `V = V0 + a * t`.  
+
+    Parameters
+    ----------
+    V0 : float or pint.Quantity  
+        Initial velocity of an object (velocity at time `t=0`), by default in meters per second (m/s)  
+    a : float or pint.Quantity  
+        Acceleration of an object, by default in meters per second squared (m/s^2).  
+    t : float or pint.Quantity  
+        Time at which we want to calculate the velocity, nonnegative, by default in seconds (s).  
+
+    Returns
+    -------
+    pint.Quantity  
+        Velocity at time `t`, in meters per second (m/s).
+
+    Raises
+    ------
+    ValueError  
+        If time `t` is negative.  
+    pint.errors.DimensionalityError  
+        If input has incompatible units
     """
-    Calculate the velocity of an object at time `t`.
-    
-    **Formula:** `V = V0 + a * t`
-    
-    **Parameters:**
+        
+    if not isinstance(V0, _si.Quantity):
+        V0 = V0 * _si.m / _si.s
+    V0 = V0.to(_si.m / _si.s)
 
-    * **V0** (float | pint.Quantity): Initial velocity of the object `V0` at time `t = 0`.
-        Default: meters per second (m/s).
-    * **a** (float | pint.Quantity): Constant acceleration of the object.
-        Default: meters per second squared (m/s^2).
-    * **t** (float | pint.Quantity): Time at which we want to calculate the velocity, nonnegative.
-        Default: seconds (s).
-    
-    **Returns:**
-    
-    * **pint.Quantity**: Velocity at time `t`, in meters per second (m/s).
-  
-    **Raises:**
-    
-    * **ValueError**: If time `t` is negative.
-    * **pint.errors.DimensionalityError**: If any input has incompatible units
-    """
-    
-    
-    if not isinstance(V0, si.Quantity):
-        V0 = V0 * si.m / si.s
-    V0 = V0.to(si.m / si.s)
+    if not isinstance(a, _si.Quantity):
+        a = a * _si.m / (_si.s**2)
+    a  = a.to(_si.m / _si.s**2)
 
-    if not isinstance(a, si.Quantity):
-        a = a * si.m / (si.s**2)
-    a  = a.to(si.m / si.s**2)
-
-    if not isinstance(t, si.Quantity):
-        t = t * si.s
-    t  = t.to(si.s)
+    if not isinstance(t, _si.Quantity):
+        t = t * _si.s
+    t  = t.to(_si.s)
 
     if t.magnitude < 0:
         raise ValueError("Time cannot be a negative number")
